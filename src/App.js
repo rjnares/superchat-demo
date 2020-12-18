06f77b0df421a7147a495338c11e07156d7a59fc
rjnares <roberto.nares@gmail.com>
2020-12-18 12:49:07 -0800

    Imported firebase sdk/hooks and created signin/singout functional components. Layed out skeleton for main App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// Firebase SDK
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Firebase Hooks
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDKcav46dWg2D-nldnWtcGTPMu07-jYUvo",
+  authDomain: "superchat-dev-8a539.firebaseapp.com",
+  projectId: "superchat-dev-8a539",
+  storageBucket: "superchat-dev-8a539.appspot.com",
+  messagingSenderId: "129583296711",
+  appId: "1:129583296711:web:5b95555a8dede24d2a4ee2",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
+}
+
+function SignIn() {
+  const googleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return <button onClick={googleSignIn}>Google Sign in</button>;
+}
+
+function SignOut() {
+  const googleSignOut = () => auth.signOut();
+
+  return auth.currentUser && <button onClick={googleSignOut}>Sign out</button>;
 }
 
 export default App;
